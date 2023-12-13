605174f1ad8dae968ecb3e2316dcd974e4433265
falahahamad <ahamadfalah.dev@gmail.com>
2023-12-14 03:56:01 +0530

    feat: implement signup

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Signup = () => {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmationRef = useRef();
+   const {setUser, setToken} = useStateContext();
 
-  const onSubmit = ev => {
-    ev.preventDefault()
+  function onSubmit(ev) {
+    ev.preventDefault();
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value,
+    };
 
+    axiosClient.post('/signup', payload).then(({ data }) => {
+      setToken(data.token)
+      setUser(data.user)
+    })
+
+    .catch(err => {
+      const response = err.response;
+      if(response && response.status == 422){
+        console.log(response.data.errors);
+        
+      }
+    })
+
+    console.log(payload);;
   }
 
   return (
@@ -14,12 +41,18 @@ const Signup = () => {
         <form onSubmit={onSubmit}>
           <h1 className="title">Signup for Free</h1>
 
-          <input  type="text" placeholder="Full Name"/>
-          <input  type="email" placeholder="Email Address"/>
-          <input  type="password" placeholder="Password"/>
-          <input  type="password" placeholder="Repeat Password"/>
+          <input ref={nameRef} type="text" placeholder="Full Name" />
+          <input ref={emailRef} type="email" placeholder="Email Address" />
+          <input ref={passwordRef} type="password" placeholder="Password" />
+          <input
+            ref={passwordConfirmationRef}
+            type="password"
+            placeholder="Repeat Password"
+          />
           <button className="btn btn-block">Signup</button>
-          <p className="message">Already registered? <Link to="/login">Sign In</Link></p>
+          <p className="message">
+            Already registered? <Link to="/login">Sign In</Link>
+          </p>
         </form>
       </div>
     </div>
