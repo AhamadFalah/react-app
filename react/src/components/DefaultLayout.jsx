0a6f5fb4bfe10855c616707f0d8ea0079ae629a3
falahahamad <ahamadfalah.dev@gmail.com>
2023-12-13 13:57:01 +0530

    feat: create protected routes

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const DefaultLayout = () => {
+  const {user, token} = useStateContext()
+
+  if(!token) {
+    return <Navigate to="/login"/>
+  }
   return (
     <div>
       Default Layout
--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const GuestLayout = () => {
+  const {token} = useStateContext()
+  if(token) {
+    return <Navigate to="/" />
+  }
   return (
     <div>
         For Guest Users Only
