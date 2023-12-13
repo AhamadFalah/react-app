31f11c040124ebe0cc2ebc495f33379c4f13d6f9
falahahamad <ahamadfalah.dev@gmail.com>
2023-12-13 11:52:43 +0530

    feat: initialized context provider for react app

---
+++ react/src/contexts/ContextProvider.jsx
@@ -0,0 +1,36 @@
+import { Children, createContext, useContext, useState } from "react";
+
+const StateContext = createContext({
+  user: null,
+  toker: null,
+  setUser: () => {},
+  setToken: () => {}
+});
+
+export const ContextProvider = ({ childern }) => {
+  const [user, setUser] = useState({});
+  const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
+
+  const setToken = (token) => {
+    _setToken(token);
+    if (token) {
+      localStorage.setItem("ACCESS_TOKEN", token);
+    } else {
+      localStorage.removeItem("ACCESS_TOKEN");
+    }
+  };
+  return (
+    <StateContext.Provider
+      value={{
+        user,
+        token,
+        setUser,
+        setToken,
+      }}
+    >
+      {Children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext)
