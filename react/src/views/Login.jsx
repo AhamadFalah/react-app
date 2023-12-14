321e75bb64729ac6a81dac4c7b7ea6920a60408d
falahahamad <ahamadfalah.dev@gmail.com>
2023-12-14 06:54:43 +0530

    feat: implement login

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from "react-router-dom";
+import {createRef, useState} from "react";
+import axiosClient from "../axios-client.js";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 const Login = () => {
+  const emailRef = createRef()
+  const passwordRef = createRef()
+  const {setUser, setToken} = useStateContext()
+  const [errors, setErrors] = useState(null)
 
   const onSubmit = ev => {
     ev.preventDefault()
 
+    const payload = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+
+      }
+      setErrors(null)
+    axiosClient.post('/login', payload)
+      .then(({data}) => {
+        setUser(data.user)
+        setToken(data.token);
+      })
+      .catch(err => {
+        const response = err.response;
+        if (response && response.status === 422) {
+          if(response.data.errors){
+            setErrors(response.data.errors)
+          } else {
+            setErrors({
+              email:[response.data.message]
+            })
+          }
+        }
+      })
   }
 
   return (
@@ -13,9 +42,15 @@ const Login = () => {
       <div className="form">
         <form onSubmit={onSubmit}>
           <h1 className="title">Login into your account</h1>
-
-          <input type="email" placeholder="Email"/>
-          <input type="password" placeholder="Password"/>
+          {errors &&
+            <div className="alert">
+              {Object.keys(errors).map(key => (
+                <p key={key}>{errors[key][0]}</p>
+              ))}
+            </div>
+          }
+          <input ref={emailRef} type="email" placeholder="Email"/>
+          <input ref={passwordRef} type="password" placeholder="Password"/>
           <button className="btn btn-block">Login</button>
           <p className="message">Not registered? <Link to="/signup">Create an account</Link></p>
         </form>
